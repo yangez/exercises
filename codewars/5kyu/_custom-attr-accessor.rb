292d99b61a66b89667fcd16cc784f278e736c565
Eric Yang <e@eric-y.com>
2015-12-28 05:15:47 -0600

    Finished attr accessor problem

--- codewars/5kyu/_custom-attr-accessor.rb
+++ codewars/5kyu/_custom-attr-accessor.rb
@@ -1,1 +1,24 @@
 # http://www.codewars.com/kata/custom-attr-accessor
+
+class SuperFoo
+
+  attr_accessor :data
+
+  def initialize
+    @data = {}
+  end
+
+  def self.data_accessor(*args)
+    args.each do |arg|
+      define_method(arg) { @data[name] }
+      define_method("#{arg}=") { |val| @data[name] = val }
+    end
+  end
+
+end
+
+class SubFoo < SuperFoo
+  data_accessor :fizz, :pi
+end
+
+@sub_foo = SubFoo.new
